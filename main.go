9b51ffcfdd2f2c447f8b7091371ef168811f7685
Taylor Monacelli <taylormonacelli@gmail.com>
2025-10-02 21:23:29 -0700

    timestamp in log output is not helpful

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/gkwa/kaleidoscopickitten/cmd"
@@ -8,10 +9,16 @@ import (
 )
 
 func init() {
+	// Configure go-logging
 	backend := logging.NewLogBackend(os.Stderr, "", 0)
-	backendLeveled := logging.AddModuleLevel(backend)
+	format := logging.MustStringFormatter(`%{message}`)
+	backendFormatted := logging.NewBackendFormatter(backend, format)
+	backendLeveled := logging.AddModuleLevel(backendFormatted)
 	backendLeveled.SetLevel(logging.ERROR, "")
 	logging.SetBackend(backendLeveled)
+
+	// Configure standard log package (used by yq)
+	log.SetFlags(0)
 }
 
 func main() {
